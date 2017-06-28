b2ce74762c7cc4a5658b3f52e920a26fb2091bc7
Elizabeth Ann Helfenberger <ehelfenb@uiowapc.cern.ch>
2017-06-28 13:46:00 +0200

    changes

--- Root/tH2017.cxx
+++ Root/tH2017.cxx
@@ -7,17 +7,43 @@ void tH2017::Init()
 {
   addRegions({"SR1lep0b","SR1lep1b","SR1lep2b","SR1lep3b","SR1lep4b"});
   
-  addHistogram("fwdJetEta0", 25,0,4.); 
-  addHistogram("fwdJetEta1", 25,0,4.); 
-  addHistogram("fwdJetEta2", 25,0,4.); 
-  addHistogram("fwdJetEta3", 25,0,4.); 
-  addHistogram("fwdJetEta4", 25,0,4.);
+  //the most forward jets for each signal region
+  addHistogram("1stfwdJetEta0", 25,0,4.); 
+  addHistogram("1stfwdJetEta1", 25,0,4.); 
+  addHistogram("1stfwdJetEta2", 25,0,4.); 
+  addHistogram("1stfwdJetEta3", 25,0,4.); 
+  addHistogram("1stfwdJetEta4", 25,0,4.);
 
-  addHistogram("fwdBJetEta0", 25,0,4.); 
-  addHistogram("fwdBJetEta1", 25,0,4.); 
-  addHistogram("fwdBJetEta2", 25,0,4.); 
-  addHistogram("fwdBJetEta3", 25,0,4.); 
-  addHistogram("fwdBJetEta4", 25,0,4.);
+  addHistogram("2ndfwdJetEta0", 25,0,4.); 
+  addHistogram("2ndfwdJetEta1", 25,0,4.); 
+  addHistogram("2ndfwdJetEta2", 25,0,4.); 
+  addHistogram("2ndfwdJetEta3", 25,0,4.); 
+  addHistogram("2ndfwdJetEta4", 25,0,4.);
+
+  addHistogram("3rdfwdJetEta0", 25,0,4.); 
+  addHistogram("3rdfwdJetEta1", 25,0,4.); 
+  addHistogram("3rdfwdJetEta2", 25,0,4.); 
+  addHistogram("3rdfwdJetEta3", 25,0,4.); 
+  addHistogram("3rdfwdJetEta4", 25,0,4.);
+
+  //the most forward bjets for each signal region
+  addHistogram("1stfwdBJetEta0", 25,0,4.); 
+  addHistogram("1stfwdBJetEta1", 25,0,4.); 
+  addHistogram("1stfwdBJetEta2", 25,0,4.); 
+  addHistogram("1stfwdBJetEta3", 25,0,4.); 
+  addHistogram("1stfwdBJetEta4", 25,0,4.);
+
+  addHistogram("2ndfwdBJetEta0", 25,0,4.); 
+  addHistogram("2ndfwdBJetEta1", 25,0,4.); 
+  addHistogram("2ndfwdBJetEta2", 25,0,4.); 
+  addHistogram("2ndfwdBJetEta3", 25,0,4.); 
+  addHistogram("2ndfwdBJetEta4", 25,0,4.);
+
+  addHistogram("3rdfwdBJetEta0", 25,0,4.); 
+  addHistogram("3rdfwdBJetEta1", 25,0,4.); 
+  addHistogram("3rdfwdBJetEta2", 25,0,4.); 
+  addHistogram("3rdfwdBJetEta3", 25,0,4.); 
+  addHistogram("3rdfwdBJetEta4", 25,0,4.);
 
   addHistogram("MET_nocuts",100,0,1000);
   addHistogram("Njets_nocuts",10,-0.5,9.5);
@@ -77,19 +103,51 @@ void tH2017::ProcessEvent(AnalysisEvent *event)
   fill("Nbjets",nBjets);
   fill("pTL",signalLeptons.at(0).Pt());
 
-  // Find forward jets/bjets
-  // Find forward jets  
+  // Find 1st,2nd,3rd most forward jets
   double fwd_eta[3]; for(int j=0;j<3;j++) fwd_eta[j]=-999; 
+  int fwdJet_idx[3]; for(int j=0;j<3;j++) fwdJet_idx[j]=-1;
   for(int iJet=0;iJet<numSignalJets;iJet++){ 
     if(fabs(signalJets[iJet].Eta())>fwd_eta[0]){
+      fwdJet_idx[0]=iJet;
       fwd_eta[0]=fabs(signalJets[iJet].Eta());
     }
   }
+  for(int iJet=0;iJet<numSignalJets;iJet++){ 
+    if(iJet==fwdJet_idx[0]) continue; 
+    if(fabs(signalJets[iJet].Eta())>fwd_eta[1]){
+      fwdJet_idx[1]=iJet;
+      fwd_eta[1]=fabs(signalJets[iJet].Eta());
+    }
+  }
+  for(int iJet=0;iJet<numSignalJets;iJet++){ 
+    if(iJet==fwdJet_idx[0]||iJet==fwdJet_idx[1]) continue; 
+    if(fabs(signalJets[iJet].Eta())>fwd_eta[2]){
+      fwdJet_idx[2]=iJet;
+      fwd_eta[2]=fabs(signalJets[iJet].Eta());
+    }
+  }
 
+  //Find 1st,2nd,3rd most forward bjets
   double fwd_bjet_eta[3]; for(int j=0;j<3;j++) fwd_bjet_eta[j]=-999;  
+  int fwdBJet_idx[3]; for(int j=0;j<3;j++) fwdBJet_idx[j]=-1;
   for(int iJet=0;iJet<nBjets;iJet++){ 
     if(fabs(bjets[iJet].Eta())>fwd_bjet_eta[0]){
+      fwdBJet_idx[0]=iJet;
       fwd_bjet_eta[0]=fabs(bjets[iJet].Eta());
+    }
+  }
+  for(int iJet=0;iJet<nBjets;iJet++){ 
+    if(iJet==fwdBJet_idx[0]) continue; 
+    if(fabs(bjets[iJet].Eta())>fwd_bjet_eta[1]){
+      fwdBJet_idx[1]=iJet;
+      fwd_bjet_eta[1]=fabs(bjets[iJet].Eta());
+    }
+  }
+  for(int iJet=0;iJet<nBjets;iJet++){ 
+    if(iJet==fwdBJet_idx[0]||iJet==fwdBJet_idx[1]) continue; 
+    if(fabs(bjets[iJet].Eta())>fwd_bjet_eta[2]){
+      fwdBJet_idx[2]=iJet;
+      fwd_bjet_eta[2]=fabs(bjets[iJet].Eta());
     }
   }
 
@@ -99,28 +157,53 @@ void tH2017::ProcessEvent(AnalysisEvent *event)
     // b-Tag signal regions
     if (nBjets==0){
       //accept("SR1lep0b");
-      fill("fwdJetEta0", fwd_eta[0]);
-      fill("fwdBJetEta0", fwd_bjet_eta[0]);
+      fill("1stfwdJetEta0", fwd_eta[0]);
+      fill("2ndfwdJetEta0", fwd_eta[1]);
+      fill("3rdfwdJetEta0", fwd_eta[2]);
+
+      fill("1stfwdBJetEta0", fwd_bjet_eta[0]);
+      fill("2ndfwdBJetEta0", fwd_bjet_eta[1]);
+      fill("3rdfwdBJetEta0", fwd_bjet_eta[2]);
     }
     if (nBjets==1){
       //accept("SR1lep1b");
-      fill("fwdJetEta1", fwd_eta[0]);
-      fill("fwdBJetEta1", fwd_bjet_eta[0]);
+      fill("1stfwdJetEta1", fwd_eta[0]);
+      fill("2ndfwdJetEta1", fwd_eta[1]);
+      fill("3rdfwdJetEta1", fwd_eta[2]);
+
+      fill("1stfwdBJetEta1", fwd_bjet_eta[0]);
+      fill("2ndfwdBJetEta1", fwd_bjet_eta[1]);
+      fill("3rdfwdBJetEta1", fwd_bjet_eta[2]);
     }
     if (nBjets==2){
       //accept("SR1lep2b");
-      fill("fwdJetEta2", fwd_eta[0]);
-      fill("fwdBJetEta2", fwd_bjet_eta[0]);
+      fill("1stfwdJetEta2", fwd_eta[0]);
+      fill("2ndfwdJetEta2", fwd_eta[1]);
+      fill("3rdfwdJetEta2", fwd_eta[2]);
+
+      fill("1stfwdBJetEta2", fwd_bjet_eta[0]);
+      fill("2ndfwdBJetEta2", fwd_bjet_eta[1]);
+      fill("3rdfwdBJetEta2", fwd_bjet_eta[2]);
     }
     if (nBjets==3){
       //accept("SR1lep3b");
-      fill("fwdJetEta3", fwd_eta[0]);
-      fill("fwdBJetEta3", fwd_bjet_eta[0]);
+      fill("1stfwdJetEta3", fwd_eta[0]);
+      fill("2ndfwdJetEta3", fwd_eta[1]);
+      fill("3rdfwdJetEta3", fwd_eta[2]);
+
+      fill("1stfwdBJetEta3", fwd_bjet_eta[0]);
+      fill("2ndfwdBJetEta3", fwd_bjet_eta[1]);
+      fill("3rdfwdBJetEta3", fwd_bjet_eta[2]);
     }
     if (nBjets==4){
       //accept("SR1lep4b");
-      fill("fwdJetEta4", fwd_eta[0]);
-      fill("fwdBJetEta4", fwd_bjet_eta[0]);
+      fill("1stfwdJetEta4", fwd_eta[0]);
+      fill("2ndfwdJetEta4", fwd_eta[1]);
+      fill("3rdfwdJetEta4", fwd_eta[2]);
+
+      fill("1stfwdBJetEta4", fwd_bjet_eta[0]);
+      fill("2ndfwdBJetEta4", fwd_bjet_eta[1]);
+      fill("3rdfwdBJetEta4", fwd_bjet_eta[2]);
     }
   }
 
